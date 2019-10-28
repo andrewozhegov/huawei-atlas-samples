a24abf73d3894b4523dba0c0007350a9fef6884f
andrewozhegov <andrew.ozhegov@yandex.ru>
2019-10-28 17:40:01 +0700

    using all devices in separate threads

--- Samples/InferOfflineVideo/main.cpp
+++ Samples/InferOfflineVideo/main.cpp
@@ -36,10 +36,11 @@
 #include <iostream>
 #include <libgen.h>
 #include <unistd.h>
+#include <thread>
 
-static const std::string GRAPH_FILENAME = "./graph.config";
+static const std::string GRAPH_FILENAME[] = { "./graph0.config", "./graph1.config", "./graph2.config", "./graph3.config"};
 // graph id
-static const uint32_t GRAPH_ID = 100;
+static const uint32_t GRAPH_ID[] = { 100, 101, 102, 103 };
 // destination engine id
 static const uint32_t SRC_ENGINE = 101;
 // leaf node engine id
@@ -56,28 +57,32 @@ HIAI_StatusT CustomDataRecvInterface::RecvData(const std::shared_ptr<void>& mess
 }
 
 // Init and create graph
-HIAI_StatusT HIAI_InitAndStartGraph(const std::string& configFile)
+HIAI_StatusT HIAI_InitAndStartGraph(const std::string& configFile, const uint32_t deviceID)
 {
     // Step1: Global System Initialization before using HIAI Engine
-    HIAI_StatusT status = HIAI_Init(0);
+    HIAI_StatusT status = HIAI_Init(deviceID);
 
+    if (status != HIAI_OK) {
+            printf("Failed to Init device %u\n", deviceID);
+    }
     // Step2: Create and Start the Graph
     status = hiai::Graph::CreateGraph(configFile);
     if (status != HIAI_OK) {
         HIAI_ENGINE_LOG(status, "Fail to start graph");
+        printf("Failed to start graph %s on device %u\n", configFile.c_str(), deviceID);
         return status;
     }
 
     // Step3: Set DataRecv Functor
-    std::shared_ptr<hiai::Graph> graph = hiai::Graph::GetInstance(GRAPH_ID);
+    std::shared_ptr<hiai::Graph> graph = hiai::Graph::GetInstance(GRAPH_ID[deviceID]);
     if (graph == nullptr) {
-        HIAI_ENGINE_LOG("Fail to get the graph-%u", GRAPH_ID);
+        HIAI_ENGINE_LOG("Fail to get the graph-%u", GRAPH_ID[deviceID]);
         return status;
     }
 
     for (int i = 0; i < sizeof(terminators) / sizeof(uint32_t); i++) {
         hiai::EnginePortID target_port_config;
-        target_port_config.graph_id = GRAPH_ID;
+        target_port_config.graph_id = GRAPH_ID[deviceID];
         target_port_config.engine_id = terminators[i];
         target_port_config.port_id = 0;
         graph->SetDataRecvFunctor(target_port_config,
@@ -90,8 +95,10 @@ void my_handler(int s)
 {
     printf("Caught signal %d\n", s);
     if (s == 2) {
-        printf("DestroyGraph %u\n", GRAPH_ID);
-        hiai::Graph::DestroyGraph(GRAPH_ID);
+        for(uint32_t n = 0; n < 4; n++) {
+            printf("DestroyGraph %u\n", GRAPH_ID[n]);
+            hiai::Graph::DestroyGraph(GRAPH_ID[n]);
+        }
         exit(0);
     }
 }
@@ -111,39 +118,51 @@ int main(int argc, char* argv[])
     }
 
     // init Graph
-    HIAI_StatusT ret = HIAI_InitAndStartGraph(GRAPH_FILENAME);
-    if (ret != HIAI_OK) {
-        printf("[main] Fail to start graph\n");
-        return -1;
+    uint32_t threads_n = 4;
+    std::thread threads[threads_n];
+    for (uint32_t n = 0; n < threads_n; n++) {
+        HIAI_StatusT ret;
+        threads[n] = std::thread([&ret, GRAPH_FILENAME, n](){
+            printf("%s %u \n", GRAPH_FILENAME[n].c_str(), n);
+            ret = HIAI_InitAndStartGraph(GRAPH_FILENAME[n], n);
+            if (ret != HIAI_OK) {
+                printf("Fail to start graph %u\n", n);
+                return -1;
+            }
+    
+            // send data
+            std::shared_ptr<hiai::Graph> graph = hiai::Graph::GetInstance(GRAPH_ID[n]);
+            if (nullptr == graph) {
+                printf("Fail to get the graph-%u\n", GRAPH_ID[n]);
+                return -1;
+            }
+            hiai::EnginePortID engine_id;
+            engine_id.graph_id = GRAPH_ID[n];
+            engine_id.engine_id = SRC_ENGINE;
+            engine_id.port_id = 0;
+            std::shared_ptr<std::string> src_data(new std::string());
+
+            graph->SendData(engine_id, "string", std::static_pointer_cast<void>(src_data));
+
+            // wait for ctrl+c
+            struct sigaction sigIntHandler;
+            sigIntHandler.sa_handler = my_handler;
+            sigemptyset(&sigIntHandler.sa_mask);
+            sigIntHandler.sa_flags = 0;
+            sigaction(SIGINT, &sigIntHandler, NULL);
+
+            while (g_flag > 0) {
+                usleep(10000);
+            }
+
+            // end
+            hiai::Graph::DestroyGraph(GRAPH_ID[n]);
+            printf("[main] destroy graph-%u done\n", GRAPH_ID[n]);
+
+        });
     }
 
-    // send data
-    std::shared_ptr<hiai::Graph> graph = hiai::Graph::GetInstance(GRAPH_ID);
-    if (nullptr == graph) {
-        printf("Fail to get the graph-%u\n", GRAPH_ID);
-        return -1;
-    }
-    hiai::EnginePortID engine_id;
-    engine_id.graph_id = GRAPH_ID;
-    engine_id.engine_id = SRC_ENGINE;
-    engine_id.port_id = 0;
-    std::shared_ptr<std::string> src_data(new std::string());
+    for (uint32_t n = 0; n < threads_n; n++) threads[n].join();
 
-    graph->SendData(engine_id, "string", std::static_pointer_cast<void>(src_data));
-
-    // wait for ctrl+c
-    struct sigaction sigIntHandler;
-    sigIntHandler.sa_handler = my_handler;
-    sigemptyset(&sigIntHandler.sa_mask);
-    sigIntHandler.sa_flags = 0;
-    sigaction(SIGINT, &sigIntHandler, NULL);
-
-    while (g_flag > 0) {
-        usleep(10000);
-    }
-
-    // end
-    hiai::Graph::DestroyGraph(GRAPH_ID);
-    printf("[main] destroy graph-%u done\n", GRAPH_ID);
     return 0;
 }
